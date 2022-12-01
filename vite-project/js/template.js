cda34181911d3d24971234429f68d230690ddfe5
dtscott9 <dtscott9@byui.edu>
2022-11-30 17:28:15 -0700

    added planet and ship select

--- vite-project/js/template.js
+++ vite-project/js/template.js
@@ -1,22 +1,50 @@
 import { fetchJson } from "./character";
 
 const charSelect = document.querySelector("#characterSelect");
+const planetSelect = document.querySelector("#planetSelect");
+const shipSelect = document.querySelector("#starshipSelect");
+const apiUrl = "https://swapi.py4e.com/api/";
 
+//Json src
 const res = await fetchJson();
+//API extensions
+const planetsPath = "planets";
+const shipsPath = "starships";
 
-function fillSelectMenu(objects) {
+function fillSelectMenu(objects, select) {
 
     objects.forEach(object => {
         const newOption = document.createElement("option");
         newOption.setAttribute("value", object.name);
         newOption.textContent = object.name;
-        charSelect.appendChild(newOption);
+        select.appendChild(newOption);
 
     })
 }
 
-function connectToSwapi() {
-
+async function apiConnect(ext) {
+    try {
+        const response = await fetch(apiUrl + ext);
+        if(response.ok){
+            return await response.json();
+        }
+        else {
+            const error = await response.text()
+            throw new Error(error);
+        }
+    }
+    catch (err){
+        console.log(err);
+    }
 }
 
-fillSelectMenu(res);
+const planetDetails = await apiConnect(planetsPath);
+const shipDetails = await apiConnect(shipsPath);
+const planets = planetDetails.results;
+const ships = shipDetails.results;
+
+
+
+fillSelectMenu(res, charSelect);
+fillSelectMenu(planets, planetSelect);
+fillSelectMenu(ships, shipSelect);
